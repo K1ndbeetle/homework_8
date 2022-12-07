1ee220a74caeaa6fd953f1a7477b4fff0b2e9be3
K1ndbeetle <alx-s@bk.ru>
2022-12-07 22:56:52 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task №1
+        System.out.println("Задача №1");
+        int[] arr = generateRandomArray();
+        int amount = 0;
+        for (int i : arr) {
+            amount += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + amount + " рублей.");
+        System.out.println();
+    }
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
